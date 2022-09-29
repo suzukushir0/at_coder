ad37a4d67b8f4c246e9007efe72f624650707817
suzukushir0 <spiraleht0413@gmail.com>
2022-09-29 23:04:01 +0900

    add ABC045-A, ABC046-B

---
+++ past_exam/Rank_A/ABC046/main.py
@@ -0,0 +1,8 @@
+print(len(list(set(map(int, input().split())))))
+
+# list = list(map(int, input().split()))
+# ans = []
+# for i in list:
+#     if i not in ans:
+#         ans.append(i)
+# print(len(ans))
