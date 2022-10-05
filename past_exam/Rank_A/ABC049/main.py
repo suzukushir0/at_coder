80f97ab16aca0f02ee839562883d0144cf4d349e
suzukushir0 <spiraleht0413@gmail.com>
2022-10-05 23:19:21 +0900

    add ABC089

---
+++ past_exam/Rank_A/ABC049/main.py
@@ -0,0 +1,11 @@
+# c = input()
+# vowel = ["a", "e", "i", "o", "u"]
+# for v in vowel:
+#     if v == c:
+#         print("vowel")
+#         exit()
+# print("consonant")
+if input() in ["a", "e", "i", "o", "u"]:
+    print("vowel")
+else:
+    print("consonant")
